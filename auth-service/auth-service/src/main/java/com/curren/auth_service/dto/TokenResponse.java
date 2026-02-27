edf2a7df1a185283ba8e8eb2bf2e25a5bedf5ffb
AndrxzBV <andrxz.b@gmail.com>
2026-02-27 18:15:04 -0500

    auth-service
    Andres Bautista

---
+++ auth-service/auth-service/src/main/java/com/curren/auth_service/dto/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.curren.auth_service.dto;
+
+public class TokenResponse {
+    private String token;
+
+    public TokenResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() { return token; }
+}
---
+++ auth-service/auth-service/src/main/java/com/curren/auth_service/entity/Role.java
@@ -0,0 +1,6 @@
+package com.curren.auth_service.entity;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER
+}
